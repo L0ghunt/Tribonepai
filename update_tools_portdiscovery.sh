9aa88f924fa9e2ce5e454e18daa1fbbb90776273
Neverl0g <83802271+L0ghunt@users.noreply.github.com>
2024-10-24 19:12:02 -0300

    Update update_tools_portdiscovery.sh
    atualizado

--- update_tools_portdiscovery.sh
+++ update_tools_portdiscovery.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # Função para atualizar tools
-function update_tools() {
+echo "Start update nuclei e nuclei templates update"
 nuclei -up
+echo " next "
 nuclei -ut
+echo "Finished nuclei updates e nuclei templates updates"
+echo "Start katatana updates"
 katana -up
+echo "Finished katana updates"
+echo "Start updates dnsx"
 dnsx -up
+echo "Finished dnsx updates"
+echo"Start subfinder update"
 subfinder -up
+echo "Finished subfinder updates"
+echo "Start alterx update"
 alterx -up
+echo "Finished alterx updates"
+echo "Start httpx update"
 httpx -up
-}
+echo "Finished htppx updates"
+echo "Finished all tools updates"
